82b0e1164b4522b7da00b9028aeec8f7d060ceac
Ifera3 <144359377+Ifera3@users.noreply.github.com>
2025-02-11 11:35:07 -0800

    fished tasks 1 and 2

--- task1.py
+++ task1.py
@@ -19,3 +19,35 @@ Fred Jones      fred@city.com   6045553434      102     75 57 Street            
 Leroy Jenkins   leroy@wow.ca    2342222323      103     65 Blizzard Ave         100     
 Jen Mezei       jen@shaw.ca     6042231134      104     891 Cullen Cresc        0
 """
+
+import sqlite3
+
+file = 'dbase.db'
+connection = sqlite3.connect(file)
+cursor = connection.cursor()
+
+query = '''
+create table if not exists customerInformation (
+    id INTEGER PRIMARY KEY autoincrement,
+    name tinytext,
+    email tinytext,
+    phoneNumber int,
+    custumerID int,
+    address tinytext,
+    balance int);
+'''
+cursor.execute(query)
+
+information = [
+    ['Joe Smith','joe@gmail.com',7783341111,101,'1234 Sesame Street',0],
+    ['Fred Jones','fred@city.com',6045553434,102,'75 57 Street',0],
+    ['Leroy Jenkins','leroy@wow.ca',2342222323,103,'65 Blizzard Ave',100],
+    ['Jen Mezei','jen@shaw.ca',6042231134,104,'891 Cullen Cresc',0]]
+for i in information:
+    query = f"insert into customerInformation (name,email,phoneNumber,custumerID,address,balance) values ('{i[0]}','{i[1]}',{i[2]},{i[3]},'{i[4]}',{i[5]});"
+    cursor.execute(query)
+
+query = "select * from customerInformation;"
+cursor.execute(query)
+result = cursor.fetchall()
+print(result)
--- task2.py
+++ task2.py
@@ -21,3 +21,39 @@ Casey           cat             Siberian        8    m          true            
 Friend          cat             Domestic        4    m          false               102
 Copper          dog             Beagle          12   m          true                104
 """
+
+import sqlite3
+
+file = 'dbase.db'
+connection = sqlite3.connect(file)
+cursor = connection.cursor()
+
+query = '''
+create table if not exists pets (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        name TINYTEXT,
+        species TINYTEXT,
+        breed TINYTEXT,
+        age INTEGER,
+        gender TINYTEXT,
+        neutered BOOLEAN,
+        ownerID INTEGER);
+'''
+cursor.execute(query)
+
+info = [
+    ['Fluffy','dog','Pomeraniam',5,'m',True,101],
+    ['Benjamin','cat','Siberian',8,'m',True,103],
+    ['Casey','cat','Siberian',8,'m',True,103],
+    ['Friend','cat','Domestic',4,'m',False,102],
+    ['Copper','dog','Beagle',12,'m',True,104],
+]
+for i in info:
+    query = f"insert into pets (name,species,breed,age,gender,neutered,ownerID) values ('{i[0]}','{i[1]}','{i[2]}',{i[3]},'{i[4]}',{i[5]},{i[6]});"
+    print(query)
+    cursor.execute(query)
+
+query = "select * from pets;"
+cursor.execute(query)
+result = cursor.fetchall()
+print(result)
